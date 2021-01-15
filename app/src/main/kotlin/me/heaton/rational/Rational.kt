7e599ec444c5b632ea78872044a89ceb7d96b07d
heaton.cai <heaton.cai@myob.com>
2021-01-15 23:38:27 +1100

    finish Rational

--- app/src/main/kotlin/me/heaton/rational/Rational.kt
+++ app/src/main/kotlin/me/heaton/rational/Rational.kt
@@ -14,12 +14,12 @@ class Rational(x: Int, y: Int) {
     /**
      * Numerator
      */
-    val numer = x / g
+    private val numer = x / g
 
     /**
      * Denominator
      */
-    val denom = y / g
+    private val denom = y / g
 
     private infix fun equal(that: Rational) = numer * that.denom == denom * that.numer
 
@@ -30,11 +30,27 @@ class Rational(x: Int, y: Int) {
         return other is Rational && (this equal other)
     }
 
+    private infix fun lessThan(that: Rational) = numer * that.denom < that.numer * denom
+
+    operator fun compareTo(that: Rational) = if (this == that) 0 else if (this lessThan that) -1 else 1
+
+    operator fun unaryMinus() = Rational(-numer, denom)
+
+    operator fun plus(that: Rational) = Rational(numer * that.denom + that.numer * denom, denom * that.denom)
+
+    operator fun minus(that: Rational) = this + -that
+
+    operator fun times(that: Rational) = Rational(numer * that.numer, denom * that.denom)
+
+    operator fun div(that: Rational) = Rational(numer * that.denom, denom * that.numer)
+
+    private val sdenom by lazy { if (denom == 1) "" else "/$denom" }
+    override fun toString(): String = "$numer$sdenom"
+
     override fun hashCode(): Int {
         var result = numer
         result = 31 * result + denom
         return result
     }
-
 
 }
--- app/src/test/kotlin/me/heaton/rational/RationalTest.kt
+++ app/src/test/kotlin/me/heaton/rational/RationalTest.kt
@@ -18,6 +18,39 @@ class RationalTest : WordSpec({
             (Rational(1) == Rational(1, 1)).shouldBeTrue()
             Rational(1) shouldBe Rational(1, 1)
         }
+
+        "1/2 < 2/3" {
+            (Rational(1, 2) < Rational(2, 3)).shouldBeTrue()
+        }
+
+        "3/2 > 1" {
+            (Rational(3, 2) > Rational(1)).shouldBeTrue()
+        }
+
+        "-1/2" {
+            -Rational(1, 2) shouldBe Rational(-1, 2)
+        }
+
+        "1/6 + 1/3 = 1/2" {
+            Rational(1, 6) + Rational(1, 3) shouldBe Rational(1, 2)
+        }
+
+        "1/2 - 1/3 = 1/6" {
+            Rational(1, 2) - Rational(1, 3) shouldBe Rational(1, 6)
+        }
+
+        "1/2 * 2/3 = 1/3" {
+            Rational(1, 2) * Rational(2, 3) shouldBe Rational(1, 3)
+        }
+
+        "2/3 / 1/2 = 4/3" {
+            Rational(2, 3) / Rational(1, 2) shouldBe Rational(4, 3)
+        }
+
+        "print nicely" {
+            Rational(4, 6).toString() shouldBe "2/3"
+            Rational(4).toString() shouldBe "4"
+        }
     }
 
 })
